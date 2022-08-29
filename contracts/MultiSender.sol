b162a83d3198291f50b45383e0f8b30e51d10c13
hoangnh2 <hoangnh2@vmodev.com>
2022-08-29 16:21:55 +0700

    add

--- contracts/MultiSender.sol
+++ contracts/MultiSender.sol
@@ -8,20 +8,22 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 contract MultiSender {
     function sendMultiETH(
         address payable[] memory listReceivers,
-        uint256[] memory listAmounts
+        uint256[] memory listAmountsamountss
     ) public payable {
         uint256 totalReceivers = listReceivers.length;
-        uint256 totalAmounts;
+        uint256 totalAmountsamountss;
         for (uint256 i = 0; i < totalReceivers; i++) {
-            totalAmounts += listAmounts[i];
+            totalAmountsamountss += listAmountsamountss[i];
         }
-        require(msg.sender.balance >= totalAmounts, "Total balance not enough");
-        require(msg.value == totalAmounts, "Value not enough");
+        require(msg.value == totalAmountsamountss, "ETH Value not enough");
+        require(
+            msg.sender.balance >= totalAmountsamountss,
+            "Total balance not enough"
+        );
         for (uint256 i = 0; i < totalReceivers; i++) {
-            (bool success, bytes memory data) = listReceivers[i].call{
-                value: listAmounts[i]
+            (bool success, ) = listReceivers[i].call{
+                value: listAmountsamountss[i]
             }("");
-            delete data;
             require(
                 success,
                 string(
@@ -36,23 +38,19 @@ contract MultiSender {
     }
 
     function sendMultiERC721(
-        address addressERC721,
+        address[] memory addressERC721s,
         address[] memory listReceivers,
         uint256[] memory listTokenId
     ) public returns (bool) {
         require(listReceivers.length == listTokenId.length, "Not same length");
 
-        uint256 totalReceivers = listReceivers.length;
-        IERC721 erc721 = IERC721(addressERC721);
-
-        for (uint256 i = 0; i < totalReceivers; i++) {
+        for (uint256 i = 0; i < listReceivers.length; i++) {
+            IERC721 erc721 = IERC721(addressERC721s[i]);
             require(
                 erc721.ownerOf(listTokenId[i]) == msg.sender,
                 "Token not owned by sender"
             );
-        }
 
-        for (uint256 i = 0; i < totalReceivers; i++) {
             require(
                 erc721.getApproved(listTokenId[i]) == address(this),
                 "Not approved"
@@ -63,24 +61,22 @@ contract MultiSender {
     }
 
     function sendMultiERC20(
-        address addressERC20,
+        address[] memory addressERC20s,
         address[] memory listReceivers,
-        uint256 amount
+        uint256[] memory amounts
     ) public returns (bool) {
-        uint256 totalReceivers = listReceivers.length;
-        IERC20 erc20 = IERC20(addressERC20);
-        require(
-            erc20.balanceOf(msg.sender) >= totalReceivers * amount,
-            "Total balance not enough"
-        );
+        require(listReceivers.length == amounts.length, "Not same length");
 
-        uint256 allowance = erc20.allowance(msg.sender, address(this));
-        require(
-            allowance >= totalReceivers * amount,
-            "Check the token allowance"
-        );
-        for (uint256 i = 0; i < totalReceivers; i++) {
-            erc20.transferFrom(msg.sender, listReceivers[i], amount);
+        for (uint256 i = 0; i < listReceivers.length; i++) {
+            IERC20 erc20 = IERC20(addressERC20s[i]);
+            require(
+                erc20.balanceOf(msg.sender) >= amounts[i],
+                "Balance not enough"
+            );
+
+            uint256 allowance = erc20.allowance(msg.sender, address(this));
+            require(allowance >= amounts[i], "Check the token allowance");
+            erc20.transferFrom(msg.sender, listReceivers[i], amounts[i]);
         }
         return true;
     }
